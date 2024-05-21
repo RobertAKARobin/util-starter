18a9429cdbf4fd76a3370ecf79b246be62504d85
RobertAKARobin <contact@robertakarobin.com>
2024-05-21 10:41:55 -0500

    fix stylelint

---
+++ src/index.css.ts
@@ -0,0 +1,9 @@
+import { css } from '@robertakarobin/util/string/template.ts';
+
+// Oddly, stylelint only wants to work in `.css.ts` files when using `css` as opposed to `/*css*/`
+
+export default css`
+div {
+	color: #000000;
+}
+`;
